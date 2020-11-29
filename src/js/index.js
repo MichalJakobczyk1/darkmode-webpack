09197f5d19bd58210684363afaa1445bb80b7284
Michał Jakóbczyk <michal.jakobczyk1@wp.pl>
2020-11-30 00:55:19 +0100

    webpack grid&colors&momentClock

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import '../scss/main.scss';
+import moment from 'moment';
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -7,3 +8,10 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+const clock = document.querySelector(".main__moment--js");
+clock.innerHTML=moment().format('LTS');
+
+window.setInterval(function(){
+    clock.innerHTML=moment().format('LTS');
+  }, 1000);
